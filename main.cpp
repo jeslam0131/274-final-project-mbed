74067decd6b7e9a412dcb6df79bf1bf8301e0a9a
Manuel Valencia <manuelv@mit.edu>
2023-11-15 20:26:24 +0000

    Jessica update for her

--- main.cpp
+++ main.cpp
@@ -6,31 +6,92 @@
 #include "MotorShield.h" 
 #include "HardwareSetup.h"
 
-#define NUM_INPUTS 2
-#define NUM_OUTPUTS 4
+#define NUM_INPUTS 4
+#define NUM_OUTPUTS 5
+
+//Measured values
+float velocity = 0;
+float current = 0;
+float theta = 0;
+
+//Set values
+float current_d = 0.5;
+float kp = 0;
+float ki = 0;
+float kd =0;
+float total_error=0;
+
+//Controller values
+float volt = 0;
+float duty = 0;
 
 Serial pc(USBTX, USBRX);    // USB Serial Terminal
 ExperimentServer server;    // Object that lets us communicate with MATLAB
 Timer t;                    // Timer to measure elapsed time of experiment
+Ticker ControlLoop;         // Ticker to run current controller at high frequency
 
-QEI encoderA( PE_9, PE_11, NC, 1200, QEI::X4_ENCODING);  // MOTOR A ENCODER (no index, 1200 counts/rev, Quadrature encoding)
-QEI encoderB( PA_5,  PB_3, NC, 1200, QEI::X4_ENCODING);  // MOTOR B ENCODER (no index, 1200 counts/rev, Quadrature encoding)
-QEI encoderC( PC_6,  PC_7, NC, 1200, QEI::X4_ENCODING);  // MOTOR C ENCODER (no index, 1200 counts/rev, Quadrature encoding)
-QEI encoderD(PD_12, PD_13, NC, 1200, QEI::X4_ENCODING); // MOTOR D ENCODER (no index, 1200 counts/rev, Quadrature encoding)
+QEI encoderA(PE_9,PE_11, NC, 1200, QEI::X4_ENCODING);  // MOTOR A ENCODER (no index, 1200 counts/rev, Quadrature encoding)
+QEI encoderB(PA_5, PB_3, NC, 1200, QEI::X4_ENCODING);  // MOTOR B ENCODER (no index, 1200 counts/rev, Quadrature encoding)
+QEI encoderC(PC_6, PC_7, NC, 1200, QEI::X4_ENCODING);  // MOTOR C ENCODER (no index, 1200 counts/rev, Quadrature encoding)
+QEI encoderD(PD_12, PD_13, NC, 1200, QEI::X4_ENCODING);// MOTOR D ENCODER (no index, 1200 counts/rev, Quadrature encoding)
 
-MotorShield motorShield(12000); //initialize the motor shield with a period of 12000 clock ticks or ~20kHZ
+MotorShield motorShield(24000); // initialize the motor shield with a period of 12000 clock ticks or ~10kHZ
+
+// function to calculate motor voltage according to current control law
+void current_control() {
+    float error = 0;
+    
+    theta = encoderA.getPulses()*(6.2831/1200.0);
+    velocity = encoderA.getVelocity()*(6.2831/1200.0);
+    current = -(motorShield.readCurrentA()*(30.0/65536.0)-15.0); //read current for motor A in amps. Note: this is a slightly different current sensor so its a different conversion than last lab.            
+    current_d = (-2*theta-0.01*velocity)/0.16;
+    error = current_d - current;
+    total_error+=error;
+    if(total_error>2){
+        total_error=2;
+    }
+    
+    volt = 3.4*current_d + kp*error+ 0.16*velocity+ki*total_error; // EDIT THIS to use your current control law from Lab 2
+    
+    duty  = volt/12.0;
+    
+    if (duty >  1) {
+        duty =  1;
+    }
+    if (duty < -1) {
+        duty = -1;   
+    }
+
+    if (duty >= 0){
+        motorShield.motorAWrite(duty, 0); 
+    }
+    else if (duty < 0){
+        motorShield.motorAWrite(abs(duty), 1);
+    }
+}
+
 
 int main (void) {
     // Link the terminal with our server and start it up
     server.attachTerminal(pc);
     server.init();
     
-    // Continually get input from MATLAB and run experiments
+    // Define array to hold input parameters from MATLAB
     float input_params[NUM_INPUTS];
     pc.printf("%f",input_params[0]);
     
     while(1) {
-        if (server.getParams(input_params, NUM_INPUTS)) {
+        // Run experiment every time input parameters are received from MATLAB
+        if (server.getParams(input_params,NUM_INPUTS)) {
+            // Unpack inputs
+            kp = input_params[0];
+            ki = input_params[1];
+            current_d = input_params[2];
+            time = input_params [3];
+
+            // Run current controller at 10kHz
+            ControlLoop.attach(&current_control,0.0001);
+            
             // Setup experiment
             t.reset();
             t.start();
@@ -41,39 +102,31 @@ int main (void) {
 
             motorShield.motorAWrite(0, 0); //turn motor A off
             
-            //use the motor shield as follows:
-            //motorShield.motorAWrite(DUTY CYCLE, DIRECTION), DIRECTION = 0 is forward, DIRECTION =1 is backwards.
+            // Use the motor shield as follows:
+            // motorShield.motorAWrite(DUTY CYCLE, DIRECTION), DIRECTION = 0 is forward, DIRECTION = 1 is backwards.
              
-            // Unpack inputs
-            float d1   = input_params[0]; // Duty cycle for first second
-            float d2   = input_params[1]; // Duty cycle for second second
-
             // Run experiment
-            while (t.read() < 2) {
-                // Perform control loop logic
-                if (t.read() < 1)
-                    motorShield.motorAWrite(d1, 0); //run motor A at "v1" duty cycle and in the forward direction 
-                else
-                    motorShield.motorAWrite(d2, 0); //run motor A at "v2" duty cycle and in the forward direction 
-
-                // Form output to send to MATLAB
-                float output_data[NUM_OUTPUTS];
-                
-                output_data[0] = t.read();
-                output_data[1] = 0; // MODIFY THIS: Copy in your equation for encoder angle from part 1
-                output_data[2] = 0; // MODIFY THIS: Copy in your equation for encoder velocity from part 1
-                output_data[3] = -(motorShield.readCurrentA()*(30.0/65536.0)-15);
+            while (t.read() < time) {
+                // Perform impedance control loop logic to calculate desired current
+                current_d = (-10*theta+0.001*velocity)/0.16; // Set commanded current from impedance controller here.
                 
                 // Send data to MATLAB
-                server.sendData(output_data,NUM_OUTPUTS);
-                
-                ThisThread::sleep_for(1); //run control loop at 1kHz
+                float output_data[NUM_OUTPUTS];
+                output_data[0] = t.read();
+                output_data[1] = theta;
+                output_data[2] = velocity;
+                output_data[3] = current;
+                output_data[4] = volt;
+
+                server.sendData(output_data,NUM_OUTPUTS);              
+                ThisThread::sleep_for(1); //run outer control loop at 1kHz
             }
-            // Cleanup after experiment
+
+            // Cleanup after each experiment
+            ControlLoop.detach();
             server.setExperimentComplete();
             motorShield.motorAWrite(0, 0); //turn motor A off
         } // end if
     } // end while
+    
 } // end main
-
-// Manny wrote this
